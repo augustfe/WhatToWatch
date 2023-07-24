0bb0d624081674bec0c8e049883dc90efafa4243
August Femtehjell <augustfe@math.uio.no>
2023-07-24 20:29:27 +0200

    write_to_file works, fixed settings

--- WIP/write_to_file.py
+++ WIP/write_to_file.py
@@ -8,22 +8,25 @@ ia = Cinemagoer()
 def addToList(name: str) -> None:
     movies = ia.search_movie(name)
     correct = None
-    print('Assert correct found [y/n]')
+    print("Assert correct found [y/n]")
+
     for movie in movies:
         response = input(f"{movie['long imdb title']} [y/n]")
-        if response == 'y':
+
+        if response == "y":
             correct = movie
             break
+
         clear_output(wait=True)
 
     if correct is None:
         print("Series was not found")
 
-    with open('serier.csv', 'a') as f_object:
+    with open("serier.csv", "a") as f_object:
         writer_object = writer(f_object)
-        title = correct['title']
+
+        title = correct["title"]
         ID = correct.getID()
         writer_object.writerow([title, ID])
 
         f_object.close()
-    
